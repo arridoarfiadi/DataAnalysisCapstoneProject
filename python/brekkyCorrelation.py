bf2515777902a88287770cb878d60a710a8b56bd
Christine <christinenguyen1995gmail.com>
2018-04-25 10:43:07 -0700

    Breakfast Correlation

---
+++ python/brekkyCorrelation.py
@@ -0,0 +1,10 @@
+import pandas as pd
+#import numpy as np
+
+
+breakfast = pd.read_csv("breakfastCorr.csv", index_col=0)
+print(breakfast)
+
+#breakfast.corr()
+#print(breakfast['Cereal_Amount'] #for a specific columnm
+#print(breakfast.loc[472] #for a specific row/observation
