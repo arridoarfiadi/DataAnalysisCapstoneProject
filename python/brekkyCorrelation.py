fe11ab276b48310d61f9a909b38e54b0c865e849
Christine <christinenguyen1995gmail.com>
2018-04-25 10:55:30 -0700

    Breakfast Correlation

--- python/brekkyCorrelation.py
+++ python/brekkyCorrelation.py
@@ -8,3 +8,4 @@ print(breakfast)
 #breakfast.corr()
 #print(breakfast['Cereal_Amount'] #for a specific columnm
 #print(breakfast.loc[472] #for a specific row/observation
+#print(breakfast.loc[[472]]) #to display data in a nice pretty data frame
