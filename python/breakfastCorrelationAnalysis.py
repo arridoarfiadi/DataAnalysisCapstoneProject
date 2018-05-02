1918aee78b38415ed3b1fc62386b5677e08dd77f
Christine <36457194+cnn22@users.noreply.github.com>
2018-05-01 18:37:30 -0700

    Update breakfastCorrelationAnalysis.py

--- python/breakfastCorrelationAnalysis.py
+++ python/breakfastCorrelationAnalysis.py
@@ -1,7 +1,9 @@
 import pandas as pd 
 
-breakfast = pd.read_csv("breakfastComplete.csv", index_col=0)
+#pd.options.display.max_columns = 50
+#pd.options.display.max_rows = 50
+
+breakfast = pd.read_csv("breakfastComplete25.csv", index_col=0)
 breakfast #displays the data 
 
 breakfast.corr() #correlation martrix of breakfast items
-
