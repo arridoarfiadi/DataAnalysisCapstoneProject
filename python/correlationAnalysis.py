428357afbb44d8f71bfad6655a8d0df6b716c685
Christine <36457194+cnn22@users.noreply.github.com>
2018-05-05 01:00:43 -0700

    Update and rename breakfastCorrelationAnalysis.py to correlationAnalysis.py

--- python/correlationAnalysis.py
+++ python/correlationAnalysis.py
@@ -4,6 +4,10 @@ import pandas as pd
 #pd.options.display.max_rows = 50
 
 breakfast = pd.read_csv("breakfastComplete25.csv", index_col=0)
-breakfast #displays the data 
+party = pd.read_csv("party25.csv", index_col=0)
+
+#breakfast #displays the data for breakfast items
+#party #displays the data for party items
 
 breakfast.corr() #correlation martrix of breakfast items
+party.corr() #correlation matrix of party items
